90d928f2e21703d6ff05260117079b2fd05805fe
sjw <341922619@qq.com>
2018-04-09 11:15:29 +0800

    sjw 提交

--- service/src/main/java/com.sjw.springcloud.service/ServiceApplication.java
+++ service/src/main/java/com.sjw.springcloud.service/ServiceApplication.java
@@ -1,4 +1,4 @@
-package com.sjw.server;
+package com.sjw.springcloud.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ServerApplication {
+public class ServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		SpringApplication.run(ServiceApplication.class, args);
 	}
 }
