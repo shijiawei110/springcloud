36a74247684af06ab991ca2399f170357c8ecf40
sjw <341922619@qq.com>
2018-04-09 11:13:10 +0800

    sjw 提交

---
+++ clientOne/src/main/java/com.sjw.springcloud.clientOne/controller/Controller.java
@@ -0,0 +1,25 @@
+package com.sjw.client.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Shijiawei
+ * Date: 2018-03-26
+ * Description:
+ */
+@RestController
+public class Controller {
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println("服务提供者被消费 : "+services);
+        return services;
+    }
+}
